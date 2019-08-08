ac6fe7571e8c84136d41c4a2e73edf6f5feee9b3
Mason Hernandez <mhernandez58@mail.greenriver.edu>
2019-08-08 15:26:58 -0700

    Adding some large text for convenience.

---
+++ sw.js
@@ -0,0 +1,21 @@
+let cacheName = 'v1';
+let cacheFiles = [
+    './',
+    './index.html',
+    './js/2048.js',
+    './js/app.js',
+    './images/app_icon.png',
+    './images/apple-touch-icon.png',
+    './images/app_icon_large.png',
+    './images/left_arrow.png',
+    './images/right_arrow.png',
+    './images/up_arrow.png',
+    './images/down_arrow.png',
+    './css/2048.css',
+    'https://cdn.jsdelivr.net/npm/vue',
+    'https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.6.1/js/mdb.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.6/css/mdb.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css',
+    'https://use.fontawesome.com/releases/v5.8.1/css/all.css'
+];
